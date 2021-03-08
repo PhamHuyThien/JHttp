ed8d611cdc31b808f28de13a85b566114e14457d
PhamHuyThien <ThienDz.Dev@gmail.com>
2021-03-08 10:18:08 +0700

    delete src/test/

--- src/test/Test.java
+++
@@ -1,9 +0,0 @@
-package test;
-
-import org.http.simple.JHttp;
-
-public class Test {
-    public static void main(String[] args) {
-        System.out.println(JHttp.get("https://fb.com").body());
-    }
-}
